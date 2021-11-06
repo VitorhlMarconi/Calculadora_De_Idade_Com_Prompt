2610a446cb50a3d71400c5204a25c850ed739036
Vitor Marconi <79171283+VitorhlMarconi@users.noreply.github.com>
2021-11-06 16:46:33 -0300

    Add files via upload

---
+++ Exercício #2/js/script.js
@@ -0,0 +1,15 @@
+//Exercício #2: Calculo de idade!
+
+    // anoNascimento = prompt("Por favor informe a data do seu nascimento");
+
+    // while(anoNascimento >= 2021){
+
+    //     alert("Por favor, informe seu ano de nascimento corretamente.");
+    //     anoNascimento = prompt("Por favor informe a data do seu nascimento");
+    // }
+
+    // var anoAtual = prompt("E qual é o seu ano atual?");
+
+    // var idadeFinal = anoAtual - anoNascimento;
+
+    // alert("A idade final é: " + idadeFinal);
